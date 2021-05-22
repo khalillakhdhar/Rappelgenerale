abe3fa4715dab6ae16624d77b24643c66ce09a5b
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-22 09:30:41 +0100

    Update Transaction.java

--- src/classes/Transaction.java
+++ src/classes/Transaction.java
@@ -3,6 +3,31 @@ package classes;
 public class Transaction {
 private String titre,date;
 private float montant;
+public String getTitre() {
+	return titre;
+}
+public void setTitre(String titre) {
+	this.titre = titre;
+}
+public String getDate() {
+	return date;
+}
+public void setDate(String date) {
+	this.date = date;
+}
+public float getMontant() {
+	return montant;
+}
+public void setMontant(float montant) {
+	this.montant = montant;
+}
+public Transaction(String titre, String date, float montant) {
+	this.titre = titre;
+	this.date = date;
+	this.montant = montant;
+}
+public Transaction() {
+}
 
 
 
