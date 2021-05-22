8fea3829286906d0e4d3c7409a5a620210961d0e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-22 09:37:22 +0100

    outcomes

--- src/classes/Outcome.java
+++ src/classes/Outcome.java
@@ -1,5 +1,24 @@
 package classes;
 
-public class Outcome {
+public class Outcome extends Transaction {
+String type;
+String raison;
+public String getType() {
+	return type;
+}
+public void setType(String type) {
+	this.type = type;
+}
+public String getRaison() {
+	return raison;
+}
+public void setRaison(String raison) {
+	this.raison = raison;
+}
+public Outcome(String titre, String date, float montant, String type, String raison) {
+	super(titre, date, montant);
+	this.type = type;
+	this.raison = raison;
+}
 
 }
