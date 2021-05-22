2a372e4d999ecc00b619d96c1888448b6537a582
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-22 09:32:28 +0100

    Create Income.java

---
+++ src/classes/Income.java
@@ -0,0 +1,19 @@
+package classes;
+
+public class Income extends Transaction {
+private String source;
+private String description;
+public String getSource() {
+	return source;
+}
+public void setSource(String source) {
+	this.source = source;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+
+}
