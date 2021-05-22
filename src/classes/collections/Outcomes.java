eac5e08f4ecd18b4c0653ab7c34f9dd9c50e46c6
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-22 09:48:15 +0100

    Update Outcomes.java

--- src/classes/collections/Outcomes.java
+++ src/classes/collections/Outcomes.java
@@ -1,5 +1,24 @@
 package classes.collections;
 
-public class Outcomes {
+import java.util.ArrayList;
 
+import classes.Outcome;
+
+public class Outcomes {
+static ArrayList<Outcome> outs=new ArrayList<Outcome>();
+static void add(Outcome out)
+{
+outs.add(out);
+
+}
+static void readall()
+{
+for(Outcome out : outs)
+{
+	
+System.out.println(out.toString());
+}
+
+
+}
 }
